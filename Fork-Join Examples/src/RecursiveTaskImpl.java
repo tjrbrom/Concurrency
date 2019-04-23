2a145ba9e3883c155df13163e2460b5f3d0e6a31
NThemelis <Nikolaos.Themelis@plath.com.cy>
2019-04-23 14:01:49 +0300

    Fork/Join framework: RecursiveTask demo

---
+++ Fork-Join Examples/src/RecursiveTaskImpl.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * With RecursiveTask, the computation of each task is bound to return a value
+ * that can be united into a single result.
+ *
+ * The invokeAll() method puts tasks in a common pool and returns Futures.
+ * The primary method for awaiting completion and extracting
+ * results of a task is {@link #join}
+ */
+public class RecursiveTaskImpl extends RecursiveTask<Integer> {
+
+    static AtomicInteger total = new AtomicInteger(0);
+    int[] ints;
+
+    public RecursiveTaskImpl(int[] ints) {
+        this.ints = ints;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int[] testArr = {4, 2, 1, 4, 2, 3, 5, 9, 8, 3, 5, 11, 8};
+
+        new RecursiveTaskImpl(testArr).compute();
+
+        System.out.println(total);
+    }
+
+    @Override
+    protected Integer compute() {
+        // say we want to split the array processing, if it's too big...
+        if (ints.length > 10) {
+            return ForkJoinTask.invokeAll(splitArr())
+                    .stream()
+                    .mapToInt(ForkJoinTask::join)
+                    .sum();
+        }
+        // if the array is small, don't bother dividing...
+        else {
+            return print(ints);
+        }
+    }
+
+    // divide the collection into smaller ones, based on some logic
+    // return a collection of tasks that will be invoked by the fork/join framework
+    public Collection<RecursiveTaskImpl> splitArr() {
+
+        List<RecursiveTaskImpl> splitted = new ArrayList<>();
+
+        splitted.add(new RecursiveTaskImpl(
+                Arrays.copyOfRange(
+                        ints, 0, ints.length / 2
+                )
+        ));
+        splitted.add(new RecursiveTaskImpl(
+                Arrays.copyOfRange(
+                        ints, ints.length / 2, ints.length
+                )
+        ));
+        return splitted;
+    }
+
+    private Integer print(int[] ints) {
+        return total.addAndGet(Arrays
+                .stream(ints)
+                .sum());
+    }
+
+}
