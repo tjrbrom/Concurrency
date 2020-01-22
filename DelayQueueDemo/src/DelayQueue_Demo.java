2a7cb9aa012cb0c8719160c90c5c101674c59362
Nik Them <tjrbrom@gmail.com>
2020-01-22 20:23:03 +0200

    DelayQueue

---
+++ DelayQueueDemo/src/DelayQueue_Demo.java
@@ -0,0 +1,57 @@
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * An unbounded blocking queue of Delayed elements, in which an element can only be taken when its delay has expired
+ */
+public class DelayQueue_Demo {
+
+    public static void main(String args[]) throws InterruptedException {
+
+        DelayQueue<DelayTest> delayQueue = new DelayQueue();
+
+        DelayTest dt1 = new DelayTest("first", 10);
+        DelayTest dt2 = new DelayTest("second", 5);
+        DelayTest dt3 = new DelayTest("third", 15);
+
+        delayQueue.offer(dt1);
+        delayQueue.offer(dt2);
+        delayQueue.offer(dt3);
+
+        // after five seconds have passed, the second object will be the first to come out,
+        // because this queue sorts it's contents based on their configured delay
+        System.out.println(delayQueue.take().getName());
+        System.out.println(delayQueue.take().getName());
+        System.out.println(delayQueue.take().getName());
+    }
+
+}
+
+class DelayTest implements Delayed {
+
+    private long delay;
+    private String name;
+
+    DelayTest(String name, long delay) {
+        this.delay = System.currentTimeMillis() + delay;
+        this.name = name;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+
+        return unit.convert(delay - System.currentTimeMillis(), TimeUnit.SECONDS);
+    }
+
+    @Override
+    public int compareTo(Delayed other) {
+
+          return this.delay < ((DelayTest) other).delay ? -1 : (this.delay > ((DelayTest) other).delay ? 1 : 0);
+    }
+
+}
